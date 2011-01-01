770dd9d3ed5405b97c86c812fe827de31ae0aaa9
joao.freitas <joao.freitas@85d8052a-49ad-40b8-b331-a68f50a8d3d2>
2011-01-01 23:08:48 +0000

    Removendo arquivo debug do eclipse

--- Debug/header/classLoader.d
+++
@@ -1,9 +0,0 @@
-header/classLoader.d header/classLoader.o: ../header/classLoader.c \
- ../header/classLoader.h ../header/structures.h \
- ../header/classFileReader.h
-
-../header/classLoader.h:
-
-../header/structures.h:
-
-../header/classFileReader.h:
