f9c7c75d1a2769621f26d72c5eebe63cb39fd597
joao.freitas <joao.freitas@85d8052a-49ad-40b8-b331-a68f50a8d3d2>
2011-01-16 13:20:07 +0000

    Removendo alguns arquivos da pasta Release

--- Release/classViewer/mnemonics/mnemonics.d
+++
@@ -1,5 +0,0 @@
-classViewer/mnemonics/mnemonics.d classViewer/mnemonics/mnemonics.o: \
- ../classViewer/mnemonics/mnemonics.c \
- ../classViewer/mnemonics/mnemonics.h
-
-../classViewer/mnemonics/mnemonics.h:
