34d009fe298a9793309a4e2d08571d7825ece6e8
joao.freitas <joao.freitas@85d8052a-49ad-40b8-b331-a68f50a8d3d2>
2011-01-16 13:18:18 +0000

    Adicionando alguns arquivos no projeto

---
+++ Release/classViewer/classViewer.d
@@ -0,0 +1,25 @@
+classViewer/classViewer.d classViewer/classViewer.o: \
+ ../classViewer/classViewer.c ../classViewer/classViewer.h \
+ ../classViewer/../reader/classLoader.h \
+ ../classViewer/../reader/../structures/classFileStructures.h \
+ ../classViewer/../reader/classFileReader.h \
+ ../classViewer/../reader/attribute/attributeUtil.h \
+ ../classViewer/../reader/attribute/../../structures/classFileStructures.h \
+ ../classViewer/../reader/attribute/../classFileReader.h \
+ ../classViewer/../structures/mnemonics.h
+
+../classViewer/classViewer.h:
+
+../classViewer/../reader/classLoader.h:
+
+../classViewer/../reader/../structures/classFileStructures.h:
+
+../classViewer/../reader/classFileReader.h:
+
+../classViewer/../reader/attribute/attributeUtil.h:
+
+../classViewer/../reader/attribute/../../structures/classFileStructures.h:
+
+../classViewer/../reader/attribute/../classFileReader.h:
+
+../classViewer/../structures/mnemonics.h:
---
+++ Release/main.d
@@ -0,0 +1,27 @@
+main.d main.o: ../main.c ../reader/classLoader.h \
+ ../reader/../structures/classFileStructures.h \
+ ../reader/classFileReader.h ../reader/attribute/attributeUtil.h \
+ ../reader/attribute/../../structures/classFileStructures.h \
+ ../reader/attribute/../classFileReader.h ../classViewer/classViewer.h \
+ ../classViewer/../reader/classLoader.h \
+ ../classViewer/../structures/mnemonics.h ../structures/mnemonics.h
+
+../reader/classLoader.h:
+
+../reader/../structures/classFileStructures.h:
+
+../reader/classFileReader.h:
+
+../reader/attribute/attributeUtil.h:
+
+../reader/attribute/../../structures/classFileStructures.h:
+
+../reader/attribute/../classFileReader.h:
+
+../classViewer/classViewer.h:
+
+../classViewer/../reader/classLoader.h:
+
+../classViewer/../structures/mnemonics.h:
+
+../structures/mnemonics.h:
