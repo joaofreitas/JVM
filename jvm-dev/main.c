90a542dd93292492be2021f08e3b0ccaa0ad5019
joao.freitas <joao.freitas@85d8052a-49ad-40b8-b331-a68f50a8d3d2>
2011-09-03 19:11:42 +0000

    JVM funcionando no linux. Corrigindo erros de execução no windows e adicionando versão para devc++.

---
+++ jvm-dev/main.c
@@ -0,0 +1,46 @@
+/*
+ * main.c
+ *
+ *  Created on: 22/12/2010
+ *      Author: joaofreitas
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "reader/classLoader.h"
+#include "classViewer/classViewer.h"
+#include "structures/mnemonics.h"
+#include "engine/executeEngine.h"
+char file_name[200];
+int main(int argc, char **argv) {
+	classFileFormat *classFile;
+	int output_type; /*Saida igual a 0 -> nÃ£o mostra, saida igual a 1 -> tela*/
+
+	output_type = 0;
+	if (argc > 1) {
+		strcpy(file_name, argv[1]);
+		printf("JVM - Executando arquivo: %s\n", argv[1]);
+		if (argc > 2) {
+			printf("Arquivo gerado com sucesso apos passar o parâmetro %s\n", argv[2]);
+			if (strcmp(argv[2], "v") == 0) {
+				output_type = 1;
+			}
+		}
+	}
+	else {
+		printf("\nDigite o nome do classFile a ser executado:\n");
+		fflush(stdout);
+		scanf("%s", file_name);
+	}
+
+	classFile = loadClassFile(file_name);
+	if (output_type) {
+		inspectClassFile(classFile);
+	} else {
+		exec(classFile, file_name);
+	}
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
