4dfc7d9413a8ca27f467bfd5eca9415f3e68a539
joao.freitas <joao.freitas@85d8052a-49ad-40b8-b331-a68f50a8d3d2>
2011-09-03 19:42:04 +0000

    Adicionando testes de println

---
+++ testes/Testes/src/TestePrintLn.java
@@ -0,0 +1,10 @@
+public class TestePrintLn {
+  public static void main(String args[]){
+    int a;
+    a = 2;
+  
+    System.out.println("Teste chapolin"); 
+    System.out.println(a); 
+    System.out.println(5);     
+  }
+}
